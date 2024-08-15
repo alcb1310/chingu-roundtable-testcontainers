702ef8c28ada4a8f9ce88e611b61c50ac2417747
Andres Court <andres@andrescourt.com>
2024-08-14 22:12:49 -0500

    add mock test for get request

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,49 @@
 import request from "supertest";
 import app, { server } from "./index";
+import { getAllCats } from "./db/cats";
+
+jest.mock("./db/cats");
 
 describe("GET /", () => {
   it("should return 200", async () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toEqual(200);
   });
+});
 
-  afterAll(() => {
-    server.close();
+describe("GET /cats", () => {
+  it("should return the cats", async () => {
+    (getAllCats as jest.Mock).mockResolvedValue([
+      {
+        id: 1,
+        name: "Garfield",
+        age: 2,
+        breed: "street",
+      },
+      {
+        id: 2,
+        name: "Sylvester",
+        age: 5,
+        breed: "tabby",
+      },
+    ]);
+
+    const response = await request(app).get("/cats");
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.length).toEqual(2);
+
+    expect(response.body[0].id).toEqual(1);
+    expect(response.body[0].name).toEqual("Garfield");
+    expect(response.body[0].breed).toEqual("street");
+    expect(response.body[0].age).toEqual(2);
+
+    expect(response.body[1].id).toEqual(2);
+    expect(response.body[1].name).toEqual("Sylvester");
+    expect(response.body[1].breed).toEqual("tabby");
+    expect(response.body[1].age).toEqual(5);
   });
+});
+
+afterAll(() => {
+  server.close();
 });
